ff6b105fab02b022c2ad4dcf8b3431f4cc9e4881
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-07 19:34:45 +0100

    js update

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // script.js
+
+function getTriangleArea (a, h) {
+		if ((a <= 0) || (h <= 0)) {
+			console.log("Nieprawidłowe dane");
+}		else {
+			return a * h /2;
+			}
+}
+
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(3, 6);
+var triangle3Area = getTriangleArea(22, 48);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
